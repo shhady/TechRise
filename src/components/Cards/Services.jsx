2efb4135e3f5ad243893ddf3a0d5535f936c7cd7
shhady <shhadyse@gmail.com>
2023-09-22 23:37:11 +0300

    servives pages

--- src/components/Cards/Services.jsx
+++ src/components/Cards/Services.jsx
@@ -9,35 +9,35 @@ export default function Cards() {
     <div className='services'>
         <h2 style={{margin:'20px'}}>Services</h2>
     <div className='CardsContainer'>
-    <Link to="/Services/Front-End"><div className='card'>
+    <Link className='linkRouterDom' to="/Services/Front-End"><div className='card'>
             <h3>Front-End</h3>
             <p>User interface development for websites and applications.</p>
             </div></Link>
-        <Link to="/Services/Back-End"><div className='card'>
+        <Link className='linkRouterDom' to="/Services/Back-End"><div className='card'>
             <h3>Back-End</h3>
             <p>Server-side logic and database management for web applications.</p>
         </div></Link>
-        <Link to="/Services/IT"><div className='card'>
+        <Link className='linkRouterDom' to="/Services/IT"><div className='card'>
             <h3>IT</h3>
             <p>Information technology, managing and maintaining computer systems.</p>
             </div></Link>
-        <Link to="/Services/AWS"><div className='card'>
+        <Link className='linkRouterDom' to="/Services/AWS"><div className='card'>
             <h3>AWS</h3>
              <p>Amazon Web Services, cloud computing and hosting solutions.</p>
             </div></Link>
-        <Link to="/Services/DevOps"><div className='card'>
+        <Link className='linkRouterDom' to="/Services/DevOps"><div className='card'>
             <h3>DevOps</h3>
              <p>Development and IT operations collaboration for efficient software delivery.</p>
             </div></Link>
-        <Link to="/Services/Cyber"><div className='card'>
+        <Link className='linkRouterDom' to="/Services/Cyber"><div className='card'>
             <h3>Cyber</h3>
              <p>Cybersecurity, protecting digital systems from threats and breaches.</p>
             </div></Link>
-        <Link to="/Services/UiUx"><div className='card'>
+        <Link className='linkRouterDom' to="/Services/UiUx"><div className='card'>
             <h3>UI/UX</h3>
              <p>User Interface and User Experience design for intuitive interactions.</p>
             </div></Link>
-        <Link to="/Services/Marketing"><div className='card'>
+        <Link className='linkRouterDom' to="/Services/Marketing"><div className='card'>
             <h3>Marketing</h3>
              <p>Promoting products or services to reach and engage customers.</p>
             </div></Link>
--- src/pages/services/ServicesPage.jsx
+++ src/pages/services/ServicesPage.jsx
@@ -8,35 +8,35 @@ export default function Cards() {
         <span   style={{fontSize:"26px", fontWeight:"bold"}}>Services</span>
         </div>
     <div className='CardsContainer'>
-        <Link to="Front-End"><div className='card'>
+        <Link className='linkRouterDom' to="Front-End"><div className='card'>
             <h3>Front-End</h3>
             <p>User interface development for websites and applications.</p>
             </div></Link>
-        <Link to="Back-End"><div className='card'>
+        <Link className='linkRouterDom' to="Back-End"><div className='card'>
             <h3>Back-End</h3>
             <p>Server-side logic and database management for web applications.</p>
         </div></Link>
-        <Link to="IT"><div className='card'>
+        <Link className='linkRouterDom' to="IT"><div className='card'>
             <h3>IT</h3>
             <p>Information technology, managing and maintaining computer systems.</p>
             </div></Link>
-        <Link to="AWS"><div className='card'>
+        <Link className='linkRouterDom' to="AWS"><div className='card'>
             <h3>AWS</h3>
              <p>Amazon Web Services, cloud computing and hosting solutions.</p>
             </div></Link>
-        <Link to="DevOps"><div className='card'>
+        <Link className='linkRouterDom' to="DevOps"><div className='card'>
             <h3>DevOps</h3>
              <p>Development and IT operations collaboration for efficient software delivery.</p>
             </div></Link>
-        <Link to="Cyber"><div className='card'>
+        <Link className='linkRouterDom' to="Cyber"><div className='card'>
             <h3>Cyber</h3>
              <p>Cybersecurity, protecting digital systems from threats and breaches.</p>
             </div></Link>
-        <Link to="UiUx"><div className='card'>
+        <Link className='linkRouterDom' to="UiUx"><div className='card'>
             <h3>UI/UX</h3>
              <p>User Interface and User Experience design for intuitive interactions.</p>
             </div></Link>
-        <Link to="Marketing"><div className='card'>
+        <Link className='linkRouterDom' to="Marketing"><div className='card'>
             <h3>Marketing</h3>
              <p>Promoting products or services to reach and engage customers.</p>
             </div></Link>
